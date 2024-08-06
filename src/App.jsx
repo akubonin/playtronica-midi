e0b7985365bd7db06e709b8b1b899fd00e1246eb
akubonin <akubonin@gmail.com>
2024-08-06 12:15:34 +0200

    netlify deploy set up

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import WelcomePage from './components/WelcomePage.jsx';
--- src/components/PlayPage.jsx
+++ src/components/PlayPage.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 // src/App.jsx
 import React, { useEffect, useState } from 'react';
 import useMIDI from '../helpers/useMIDI';
